c12ed98d6acfe416bdb1c959706bbba3c0220356
Paolo <devpsalud@gmail.com>
2020-02-08 19:18:01 +0800

    Updates

---
+++ precache-manifest.897eb88b6fcee0e0778c2260f74426df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87bd8c910126dccf5e190c49567ea1ca",
+    "url": "/paogrammer/robofriends/index.html"
+  },
+  {
+    "revision": "00f841abd1e5008643e8",
+    "url": "/paogrammer/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b16b87958977cfaa1353",
+    "url": "/paogrammer/robofriends/static/css/main.03a0158d.chunk.css"
+  },
+  {
+    "revision": "00f841abd1e5008643e8",
+    "url": "/paogrammer/robofriends/static/js/2.a9290b61.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/paogrammer/robofriends/static/js/2.a9290b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b16b87958977cfaa1353",
+    "url": "/paogrammer/robofriends/static/js/main.f4ed40d7.chunk.js"
+  },
+  {
+    "revision": "0ca2cf5d443d3c0b1cfd",
+    "url": "/paogrammer/robofriends/static/js/runtime-main.3a08d1d4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/paogrammer/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
